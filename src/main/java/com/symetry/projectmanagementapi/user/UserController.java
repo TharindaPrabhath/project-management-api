c83fbd5afa880b65f6378901cbac4abf64333ea0
Tharinda P Anurajeewa <tharindahp@gmail.com>
2021-05-23 09:26:56 +0530

    created user endpoint

--- src/main/java/com/symetry/projectmanagementapi/user/UserController.java
+++ src/main/java/com/symetry/projectmanagementapi/user/UserController.java
@@ -1,9 +1,25 @@
 package com.symetry.projectmanagementapi.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1/users")
 public class UserController {
     
-    String name;
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("{userId}")
+    public User getUser(@PathVariable Long userId){
+        return userService.getUser(userId);
+    }
+    
 }
---
+++ src/main/java/com/symetry/projectmanagementapi/user/UserService.java
@@ -0,0 +1,31 @@
+package com.symetry.projectmanagementapi.user;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javassist.NotFoundException;
+
+@Service
+public class UserService {
+    
+    private final UserRepository UserRepository;
+
+    @Autowired
+    public UserService(UserRepository UserRepository) {
+        this.UserRepository = UserRepository;
+    }
+
+    public User getUser(Long userId){
+        Optional<User> user = UserRepository.findById(userId);
+        if(!user.isPresent()){
+            try {
+                throw new NotFoundException("No such a user");
+            } catch (NotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return user.get();
+    }
+}
